13a646e8264f894bb60d41601fbf1a94e29712ed
Dean Slama Jr <deanslamajr@gmail.com>
2017-11-05 17:20:25 -0600

    wip. made browser navigation work better after submitting a sample (should be able to naviagate back once from the /e/trak-name to /)

--- shared/actions/recorder.js
+++ shared/actions/recorder.js
@@ -10,7 +10,7 @@ export const RECORDER_SET_STAGED_SAMPLE = 'RECORDER_SET_STAGED_SAMPLE';
 // ACTIONS
 
 export function setStagedObjectUrl(objectUrl) {
-  return { type: RECORDER_STAGE_OBJECT_URL, objectUrl };
+  return { type: RECORDER_STAGE_OBJECT_URL, payload: objectUrl };
 }
 
 export function setStagedSample(stagedSample) {
